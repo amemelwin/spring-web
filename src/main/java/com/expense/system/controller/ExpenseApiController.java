7f6ec5f223bc80f9c229ba9540790ef63c59b70d
Ame <mrs.amemelwin@gmail.com>
2023-05-08 15:20:55 +0630

    delete api

--- src/main/java/com/expense/system/controller/ExpenseApiController.java
+++ src/main/java/com/expense/system/controller/ExpenseApiController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -107,5 +108,19 @@ public class ExpenseApiController {
 		}else {
 			return new ErrorResponse().response();			
 		}		
+	}
+	
+	@DeleteMapping("/expense/delete/{id}")
+	public ResponseEntity deleteExpense(@PathVariable int id) {
+		Expense expense = this.expenseService.getExpense(id);
+		if(expense !=null ) {
+			this.expenseService.deleteExpense(expense);
+			return new ResponseEntity<>(expense,HttpStatus.OK);
+		}else {
+			return new ErrorResponse().response();
+			
+		}
+		
+		
 	}
 }
